3d77a45a0ea5fd8049e846903547a3d36de8e581
Razi Shahid <mailrazi786@gmail.com>
2021-01-19 19:19:10 +0530

    Added the originalUrl to the session through isLoggedIn middleware

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl; //adding the original Url to the session, from where we were redirected to the login page
         req.flash('error', 'You must be signed in first')
         res.redirect('/login');
     } else {
