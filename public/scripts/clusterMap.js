bf8c474f274afa4bb28b30807c7a5f3e897458d6
Razi Shahid <mailrazi786@gmail.com>
2021-01-30 20:25:34 +0530

    Change center of cluster map from USA to India

--- public/scripts/clusterMap.js
+++ public/scripts/clusterMap.js
@@ -2,8 +2,8 @@
     const map = new mapboxgl.Map({
         container: 'cluster-map',
         style: 'mapbox://styles/mapbox/dark-v10',
-        center: [-103.59179687498357, 40.66995747013945],
-        zoom: 3
+        center: [78.9629, 20.5937],
+        zoom: 4
     });
 
     map.addControl(new mapboxgl.NavigationControl());
